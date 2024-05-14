ca5923a7c9cccfa9c0416b96eb0b5c3beb8f2fb4
miguel-faria <miguel.faria@tecnico.ulisboa.pt>
2024-05-14 18:27:18 +0100

    Updates to pursuit and lb-foraging training scripts

---
+++ legibility/scripts/run_train_pursuit_single_vdn_dqn_slurm_conda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --mail-type=BEGIN,END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=miguel.faria@tecnico.ulisboa.pt
+#SBATCH --job-name=train_pursuit_vdn
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=shard:10
+#SBATCH --time=336:00:00
+#SBATCH --mem=4G
+date;hostname;pwd
+
+export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
+export PATH="/opt/cuda/bin:$PATH"
+source "$HOME"/miniconda3/bin/activate deep_rl_env
+
+python "$HOME"/Documents/Projects/deep_rl/scripts/run_train_pursuit_single_vdn_dqn.py --field 10 --limits 1 4 --prey-type idle --hunters 2
+
+source "$HOME"/miniconda3/bin/deactivate
+date

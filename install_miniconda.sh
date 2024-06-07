c8bcc8e4be00545769acacb600d37b30b99d7fdc
Miguel Faria <miguel.faria@tecnico.ulisboa.pt>
2024-06-07 16:44:27 +0100

    Update miniconda install script to install and initialize mamba

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Download and install miniconda
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
+
+# Initialize conda
+source ~/.bashrc
+conda init
+
+# Install and initialize mamba 
+conda install -y mamba -c conda-forge
+mamba init
+source ~/.bashrc

c2862168aeb786ac8f47e2c67058173194e554a4
Miguel Faria <miguel.faria@tecnico.ulisboa.pt>
2024-07-22 16:32:29 +0100

    Refactor of train scripts

--- legibility/scripts/run_train_lb_single_vdn_dqn_slurm_conda.sh
+++ legibility/scripts/run_train_lb_single_vdn_dqn_slurm_conda.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --mail-type=BEGIN,END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=miguel.faria@tecnico.ulisboa.pt
-#SBATCH --job-name=train_lb_foraging_vdn
+#SBATCH --job-name=train_toxic_multi_model
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=shard:10
-#SBATCH --time=336:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --time=5-00:00:00
 #SBATCH --mem=4G
+#SBATCH --qos=gpu-long
 #SBATCH --output="job-%x-%j.out"
 
 date;hostname;pwd
-HOST=$(hostname)
 
 if [ -n "${SLURM_JOB_ID:-}" ] ; then
   script_path=$(dirname "$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}' | head -n 1)")
@@ -20,15 +20,15 @@ else
   script_path="$( cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P )"
 fi
 
-export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
-export PATH="/opt/cuda/bin:$PATH"
+#export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
+#export PATH="/opt/cuda/bin:$PATH"
 source "$HOME"/miniconda3/bin/activate deep_rl_env
 
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.3
 if [ "$HOSTNAME" = "artemis" ] || [ "$HOSTNAME" = "poseidon" ] ; then
-  python "$script_path"/run_train_lb_vdn_single_dqn.py --field-len 8 --logs /mnt/scratch-artemis/miguelfaria/logs/lb-foraging
+  python "$script_path"/run_train_lb_vdn_single_dqn.py --field-len 20 --batch-size 64 --buffer-size 5000 --iterations 500 --episode-steps 800 --limits 1 8  --logs-dir /mnt/scratch-artemis/miguelfaria/logs/lb-foraging --models-dir /mnt/data-artemis/miguelfaria/deep_rl/models --data-dir /mnt/data-artemis/miguelfaria/deep_rl/data --cycle-type linear --cycle-eps 0.4 --eps-type log --eps-decay 0.175 --use-lower-curriculum
 else
-  python "$script_path"/run_train_lb_vdn_single_dqn.py --field-len 20
+  python "$script_path"/run_train_lb_vdn_single_dqn.py --field-len 20 --iterations 500 --episode-steps 800 --limits 1 8  --cycle-eps 0.4 --eps-type log --eps-decay 0.175 --use-lower-curriculum
 fi
 
 source "$HOME"/miniconda3/bin/deactivate
--- legibility/scripts/run_train_lb_vdn_legible_dqn.py
+++ legibility/scripts/run_train_lb_vdn_legible_dqn.py
@@ -63,33 +63,37 @@ WARMUP_STEPS = STEPS_EPISODE * 2
 USE_RENDER = False
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--limits', dest='limits', nargs=2, type=int, required=False, default=[1, MAX_SPAWN_FOODS], help='Min and max number of food spawns to train.')
-parser.add_argument('--field-len', dest='field_len', type=int, required=False, default=FIELD_LENGTH, help='Length of the field.')
-parser.add_argument('--episode-steps', dest='max_steps', type=int, required=False, default=STEPS_EPISODE, help='Maximum number of steps per episode.')
-parser.add_argument('--iterations', dest='max_iterations', type=int, required=False, default=N_ITERATIONS, help='Number of iterations to train.')
-parser.add_argument('--logs', dest='logs', type=str, required=False, default=TENSORBOARD_DATA[0], help='Directory to store the performance logs.')
-parser.add_argument('--legible-reward', dest='legible_reward', type=str, choices=['simple', 'q_vals', 'info', 'reward'], required=False, default=LEG_REWARD,
-					help='Type of legible reward. Types: simple, q_vals, info, reward')
-parser.add_argument('--cycle-type', dest='cycle_type', type=str, required=False, default=CYCLE_TYPE, help='Type of decay for each cycle starting epsilon.')
+parser.add_argument('--batch-size', dest='batch_size', type=int, required=False, default=BATCH_SIZE)
+parser.add_argument('--buffer-method', dest='buffer_method', type=str, required=False, default='uniform', choices=['uniform', 'weighted'],
+					help='Method of deciding how to add new experience samples when replay buffer is full')
+parser.add_argument('--buffer-smart-add', dest='buffer_smart_add', action='store_true',
+					help='Flag denoting the use of smart sample add to experience replay buffer instead of first-in first-out')
+parser.add_argument('--buffer-size', dest='buffer_size', type=int, required=False, default=BUFFER)
 parser.add_argument('--cycle-eps', dest='cycle_eps', type=float, required=False, default=CYCLE_EPS, help='Epsilon decay for each cycle.')
-parser.add_argument('--eps-type', dest='eps_type', type=str, required=False, default=EPS_TYPE, help='Type of epsilon decay.')
-parser.add_argument('--eps-decay', dest='eps_decay', type=float, required=False, default=EPS_DECAY, help='Epsilon decay.')
-parser.add_argument('--models-dir', dest='models_dir', type=str, default='',
-					help='Directory to store trained models and load optimal models, if left blank stored in default location')
-parser.add_argument('--logs-dir', dest='logs_dir', type=str, default='', help='Directory to store logs, if left blank stored in default location')
+parser.add_argument('--cycle-type', dest='cycle_type', type=str, required=False, default=CYCLE_TYPE, help='Type of decay for each cycle starting epsilon.')
 parser.add_argument('--data-dir', dest='data_dir', type=str, default='',
 					help='Directory to retrieve data regarding configs and model performances, if left blank using default location')
+parser.add_argument('--eps-decay', dest='eps_decay', type=float, required=False, default=EPS_DECAY, help='Epsilon decay.')
+parser.add_argument('--eps-type', dest='eps_type', type=str, required=False, default=EPS_TYPE, help='Type of epsilon decay.')
+parser.add_argument('--episode-steps', dest='max_steps', type=int, required=False, default=STEPS_EPISODE, help='Maximum number of steps per episode.')
+parser.add_argument('--field-len', dest='field_len', type=int, required=False, default=FIELD_LENGTH, help='Length of the field.')
+parser.add_argument('--iterations', dest='max_iterations', type=int, required=False, default=N_ITERATIONS, help='Number of iterations to train.')
+parser.add_argument('--legible-reward', dest='legible_reward', type=str, choices=['simple', 'q_vals', 'info', 'reward'], required=False, default=LEG_REWARD,
+					help='Type of legible reward. Types: simple, q_vals, info, reward')
+parser.add_argument('--limits', dest='limits', nargs=2, type=int, required=False, default=[1, MAX_SPAWN_FOODS], help='Min and max number of food spawns to train.')
+parser.add_argument('--logs', dest='logs', type=str, required=False, default=TENSORBOARD_DATA[0], help='Directory to store the performance logs.')
+parser.add_argument('--logs-dir', dest='logs_dir', type=str, default='', help='Directory to store logs, if left blank stored in default location')
+parser.add_argument('--models-dir', dest='models_dir', type=str, default='',
+					help='Directory to store trained models and load optimal models, if left blank stored in default location')
 parser.add_argument('--use-lower-curriculum', dest='use_lower_model', action='store_true',
 					help='Flag that signals the use of curriculum learning with a model with one less food item spawned.')
 parser.add_argument('--use-higher-curriculum', dest='use_higher_model', action='store_true',
 					help='Flag that signals the use of curriculum learning with a model with one more food item spawned.')
-parser.add_argument('--buffer-smart-add', dest='buffer_smart_add', action='store_true',
-					help='Flag denoting the use of smart sample add to experience replay buffer instead of first-in first-out')
-parser.add_argument('--buffer-method', dest='buffer_method', type=str, required=False, default='uniform', choices=['uniform', 'weighted'],
-					help='Method of deciding how to add new experience samples when replay buffer is full')
 
 input_args = parser.parse_args()
 add_method = input_args.buffer_method
+buffer_size = input_args.buffer_size
+batch_size = input_args.batch_size
 cycle_type = input_args.cycle_type
 cycle_eps = input_args.cycle_eps
 data_dir = input_args.data_dir
@@ -119,8 +123,8 @@ for i in (reversed(range(limits[0], limits[1] + 1)) if use_higher_model else ran
 	args = (" --n-agents %d --architecture %s --buffer %d --gamma %f --beta %f --reward %s --iterations %d --max-cycles %d --batch %d --train-freq %d "
 			"--target-freq %d --alpha %f --tau %f --init-eps %f --final-eps %f --eps-decay %f --eps-type %s --warmup-steps %d --cycle-eps-decay %f --legibility-temp %f "
 			"--n-players %d --player-level %d --field-size %d --n-food %d --food-level %d --steps-episode %d --n-foods-spawn %d --tensorboardDetails %s %d %d %s"
-			% (N_AGENTS, ARQUITECTURE, BUFFER, GAMMA, BETA, leg_reward,  																					# DQN parameters
-			   iterations, MAX_CYCLES, BATCH_SIZE, TRAIN_FREQ, TARGET_FREQ, ALPHA, TAU, INIT_EPS, FINAL_EPS, decay, eps, WARMUP_STEPS, cycle_eps, TEMP,  	# Train parameters
+			% (N_AGENTS, ARQUITECTURE, buffer_size, GAMMA, BETA, leg_reward,  																				# DQN parameters
+			   iterations, MAX_CYCLES, batch_size, TRAIN_FREQ, TARGET_FREQ, ALPHA, TAU, INIT_EPS, FINAL_EPS, decay, eps, WARMUP_STEPS, cycle_eps, TEMP,  	# Train parameters
 			   N_PLAYERS, PLAYER_LEVEL, field_len, N_FOODS, FOOD_LVL, max_steps, N_SPAWN_FOODS,  															# Environment parameters
 			   tracker_logs, TENSORBOARD_DATA[1], TENSORBOARD_DATA[2], TENSORBOARD_DATA[3]))
 	args += ((" --dueling" if USE_DUELING else "") + (" --ddqn" if USE_DDQN else "") + (" --render" if USE_RENDER else "") + ("  --gpu" if USE_GPU else "") +

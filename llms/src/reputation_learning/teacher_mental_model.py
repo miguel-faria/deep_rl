456ec75bbde841f6b4db418885db52fb8077ed76
miguel-faria <miguel.faria@tecnico.ulisboa.pt>
2024-09-18 19:17:53 +0100

    Update to mental model

--- llms/src/reputation_learning/teacher_mental_model.py
+++ llms/src/reputation_learning/teacher_mental_model.py
@@ -6,6 +6,12 @@ from typing import Dict, List, Union, Tuple
 from teacher_model import TeacherModel
 from student_model import StudentModel
 from transformers import PreTrainedModel, PreTrainedTokenizer
+from model import UnidentifiedTaskError
+
+
+class UnidentifiedUtilityMetricError(Exception):
+	"""Raise exception for an intervention strategy type that is not defined"""
+	pass
 
 
 class TeacherMentalModel(TeacherModel):
@@ -43,12 +49,158 @@ class TeacherMentalModel(TeacherModel):
 					return class_scores[int(test_sample["answer"]) - 1]
 				elif self._task == 'gsm8k':
 					pass
+				else:
+					raise UnidentifiedTaskError('Task %s not defined' % self._task)
 			elif self._utility_type.find('least') != -1:
 				class_scores = student.predict_confidence(test_sample)
 				return min(class_scores)
 			elif self._utility_type.find('utility') != -1 and self._utility_type.find('correct') != -1:
+				_, explanation = self.model.predict(test_sample)
+				intervention_class_scores = student.predict_confidence(test_sample, with_explanation=True, explanation=explanation)
+				no_intervention_class_scores = student.predict_confidence(test_sample, with_explanation=False, explanation='')
+				if self._task == 'strategy_qa':
+					if test_sample["answer"] == "yes":
+						return intervention_class_scores[0] - no_intervention_class_scores[0]
+					else:
+						return intervention_class_scores[1] - no_intervention_class_scores[1]
+				elif self._task == 'ec_qa':
+					return intervention_class_scores[int(test_sample["answer"]) - 1] - no_intervention_class_scores[int(test_sample["answer"]) - 1]
+				elif self._task == 'gsm8k':
+					pass
+				else:
+					raise UnidentifiedTaskError('Task %s not defined' % self._task)
+			else:
+				raise UnidentifiedUtilityMetricError('Utility metric %s not defined' % self._utility_type)
+		
+		elif self._utility_type.find('teacher') != -1 and self._utility_type.find('confidence') != -1:
+				class_scores = self.predict_confidence(test_sample, with_explanation=True)
+				if self._task == "strategyQA":
+					return class_scores[0] if test_sample["answer"] == "yes" else class_scores[1]
+				elif self._task == "ecqa":
+					return class_scores[int(test_sample["answer"]) - 1]
+				elif self._task == 'gsm8k':
+					pass
+				else:
+					raise UnidentifiedTaskError('Task %s not defined' % self._task)
+		
+		elif (self._utility_type.find('mental') != -1 and self._utility_type.find('model') != -1) or self._utility_type.find('mm') != -1:
+			
+			if self._utility_type.find('no') != -1:
 				pass
-
-
+			
+			elif self._utility_type.find('both') != -1:
+				pass
+			
+			else:
+				pass
+		
+		else:
+			raise UnidentifiedUtilityMetricError('Utility metric %s not defined' % self._utility_type)
+	
+	
+	
+	def simulate_utility(self, test_sample: Dict, use_answers: bool):
+		no_inter_predictions, inter_predictions = [], []
+		no_inter_correct_scores, inter_correct_scores = [], []
+		if use_answers:
+			correct_answer = test_sample["answer"]
+		else:
+			teacher_prediction, _ = self.predict(test_sample)
+			correct_answer = teacher_prediction
+		
+		if self._mm_intervention == "mm_no_inter":
+			no_inter_prompt = self.prepare_prompt_no_inter(test_sample)
+			option_scores, output = self.predict_util_no_inter(no_inter_prompt, test_sample)
+			
+			print(f'AI simulated answer with no intervention (Mental Model) = {output}')
+			
+			no_inter_predictions.append(output)
+			if self._task == "strategyQA":
+				if correct_answer == "yes":
+					no_inter_correct_scores.append(option_scores[0])
+				else:
+					no_inter_correct_scores.append(option_scores[1])
+			elif self._task == "ecqa":
+				if correct_answer == "1":
+					no_inter_correct_scores.append(option_scores[0])
+				elif correct_answer == "2":
+					no_inter_correct_scores.append(option_scores[1])
+				elif correct_answer == "3":
+					no_inter_correct_scores.append(option_scores[2])
+				elif correct_answer == "4":
+					no_inter_correct_scores.append(option_scores[3])
+				else:
+					no_inter_correct_scores.append(option_scores[4])
+			elif self._task == "gsm8k":
+				no_inter_correct_scores.append(option_scores[0])
+		
+		elif self._mm_intervention == "mm_inter":
+			inter_prompt = self.prepare_prompt_inter(test_sample)
+			option_scores, output = self.predict_util_inter(inter_prompt, test_sample)
+			
+			print(f'AI simulated with teacher intervention (Mental Model) = {output}')
+			
+			inter_predictions.append(output)
+			if self._task == "strategyQA":
+				if correct_answer == "yes":
+					inter_correct_scores.append(option_scores[0])
+				else:
+					inter_correct_scores.append(option_scores[1])
+			elif self._task == "ecqa":
+				if correct_answer == "1":
+					inter_correct_scores.append(option_scores[0])
+				elif correct_answer == "2":
+					inter_correct_scores.append(option_scores[1])
+				elif correct_answer == "3":
+					inter_correct_scores.append(option_scores[2])
+				elif correct_answer == "4":
+					inter_correct_scores.append(option_scores[3])
+				else:
+					inter_correct_scores.append(option_scores[4])
+			elif self._task == "gsm8k":
+				inter_correct_scores.append(option_scores[0])
+		
+		elif self._mm_intervention == "mm_both":
+			no_inter_prompt = self.prepare_prompt_no_inter(test_sample)
+			no_inter_option_scores, no_inter_output = self.predict_util_no_inter(no_inter_prompt, test_sample)
+			
+			print(f'AI simulated answer with no intervention (Mental Model) = {no_inter_output}')
+			
+			inter_prompt = self.prepare_prompt_inter(test_sample)
+			inter_option_scores, inter_output = self.predict_util_inter(inter_prompt, test_sample)
+			
+			print(f'AI simulated answer with teacher intervention (Mental Model) = {inter_output}')
+			
+			no_inter_predictions.append(no_inter_output)
+			inter_predictions.append(inter_output)
+			
+			if self._task == "strategyQA":
+				if correct_answer == "yes":
+					no_inter_correct_scores.append(no_inter_option_scores[0])
+					inter_correct_scores.append(inter_option_scores[0])
+				else:
+					no_inter_correct_scores.append(no_inter_option_scores[1])
+					inter_correct_scores.append(inter_option_scores[1])
+			elif self._task == "ecqa":
+				if correct_answer == "1":
+					no_inter_correct_scores.append(no_inter_option_scores[0])
+					inter_correct_scores.append(inter_option_scores[0])
+				elif correct_answer == "2":
+					no_inter_correct_scores.append(no_inter_option_scores[1])
+					inter_correct_scores.append(inter_option_scores[1])
+				elif correct_answer == "3":
+					no_inter_correct_scores.append(no_inter_option_scores[2])
+					inter_correct_scores.append(inter_option_scores[2])
+				elif correct_answer == "4":
+					no_inter_correct_scores.append(no_inter_option_scores[3])
+					inter_correct_scores.append(inter_option_scores[3])
+				else:
+					no_inter_correct_scores.append(no_inter_option_scores[4])
+					inter_correct_scores.append(inter_option_scores[4])
+			elif self._task == "gsm8k":
+				no_inter_correct_scores.append(no_inter_option_scores[0])
+				inter_correct_scores.append(inter_option_scores[0])
+		
+		return no_inter_predictions, inter_predictions, no_inter_correct_scores, inter_correct_scores
 
 
--- llms/src/reputation_learning/teacher_model.py
+++ llms/src/reputation_learning/teacher_model.py
@@ -9,32 +9,32 @@ from transformers import PreTrainedModel, PreTrainedTokenizer
 
 
 class TeacherModel(Model):
-
+	
 	def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, model_name: str, expl_type: str, task: str, max_tokens: int, num_beams: int, samples: List[Dict],
 	             use_explanations: bool):
-
+		
 		super().__init__(model, tokenizer, model_name, expl_type, task, max_tokens, num_beams, samples, use_explanations)
-
-	def predict_confidence(self, test_sample: Dict, with_expl: bool = False) -> List[float]:
+	
+	def predict_confidence(self, test_sample: Dict, with_explanation: bool = False) -> List[float]:
 		context = self.get_context(test_sample)
 		tokens = self.tokenizer([context], return_tensors="pt").to("cuda")
 		generated = self.model.generate(**tokens, num_beams=self._num_beams, max_new_tokens=self._max_tokens, output_scores=True, return_dict_in_generate=True)
-
+		
 		idx = 1 if "llama" in self._model_name else 0
 		if self._task == "strategy_qa":
 			yes_id, no_id = self.tokenizer.encode("yes")[idx], self.tokenizer.encode("no")[idx]
 			answer_id = 0
-			if with_expl:
+			if with_explanation:
 				generated_tokens = generated[0].squeeze().tolist()
 				if yes_id in generated_tokens or no_id in generated_tokens:
 					answer_id = generated_tokens.index(yes_id) - 1 if yes_id in generated_tokens else generated_tokens.index(no_id) - 1
 			scores = softmax(generated['scores'][answer_id], dim=-1)
-
+			
 			yes_score, no_score = scores[0][yes_id].item(), scores[0][no_id].item()
 			print('Yes score = %s' % yes_score)
 			print('No score = %s' % no_score)
 			class_scores = [yes_score, no_score]
-
+		
 		elif self._task == "ec_qa":
 			option1_id, option2_id, option3_id, option4_id, option5_id = (self.tokenizer.encode("1")[idx], self.tokenizer.encode("2")[idx], self.tokenizer.encode("3")[idx],
 			                                                              self.tokenizer.encode("4")[idx], self.tokenizer.encode("5")[idx])
@@ -45,7 +45,7 @@ class TeacherModel(Model):
 			option5_text_id = self.tokenizer.encode(test_sample["options"][4].split(" ")[0])[idx]
 			answer_id = 0
 			found_text = False
-			if with_expl:
+			if with_explanation:
 				generated_tokens = generated[0].squeeze().tolist()
 				if option1_id in generated_tokens:
 					answer_id = self.get_answer_idx(generated_tokens, option1_id)
@@ -69,7 +69,7 @@ class TeacherModel(Model):
 						answer_id = max(answer_id, self.get_answer_idx(generated_tokens, option4_text_id))
 					if option5_text_id in generated_tokens:
 						answer_id = max(answer_id, self.get_answer_idx(generated_tokens, option5_text_id))
-
+			
 			scores = softmax(generated['scores'][answer_id], dim=-1)
 			if found_text:
 				option1_score, option2_score, option3_score, option4_score, option5_score = (scores[0][option1_text_id].item(), scores[0][option2_text_id].item(), scores[0][option3_text_id].item(),
@@ -83,12 +83,12 @@ class TeacherModel(Model):
 			print('Option4 score = %s' % option4_score)
 			print('Option5 score = %s' % option5_score)
 			class_scores = [option1_score, option2_score, option3_score, option4_score, option5_score]
-
+		
 		else:
 			raise UnidentifiedTaskError('Task %s not defined' % self._task)
-
+		
 		return class_scores
-
+	
 	def predict(self, test_sample: Dict) -> Tuple[str, str]:
 		if self._expl_type == "human":
 			return '', str(test_sample["explanation"])
@@ -97,14 +97,14 @@ class TeacherModel(Model):
 			tokens = self.tokenizer([context], return_tensors="pt").to("cuda")
 			generated = self.model.generate(**tokens, num_beams=self._num_beams, max_new_tokens=self._max_tokens)
 			output = self.tokenizer.batch_decode(generated, skip_special_tokens=True)[0].strip()
-
+		
 		if "llama" in self._model_name:
 			output = output[len(context):]
 		output = output[:output.index('\n')].strip() if '\n' in output else output.strip()
-
+		
 		if "The correct choice is " in output:
 			output = output[len("The correct choice is "):].strip()
-
+		
 		if self.expl_type == "rationalize":
 			if self._task == "ec_qa":
 				if output not in ["1", "2", "3", "4", "5"]:
@@ -116,7 +116,7 @@ class TeacherModel(Model):
 			print('%s prediction = %s' % (self._model_name, prediction))
 			explanation = " ".join(output.split(" ")[2:])
 			print('%s explanation = %s' % (self._model_name, explanation))
-
+		
 		else:
 			explanation = output[:output.rfind(".") + 1]
 			print('%s explanation = %s' % (self._model_name, explanation))
@@ -127,7 +127,7 @@ class TeacherModel(Model):
 						if choice in output:
 							prediction = str(i + 1)
 							break
-
+			
 			elif self._task == "strategy_qa":
 				if prediction not in ["no", "yes"]:
 					print("Regenerating with the explanation")
@@ -139,7 +139,7 @@ class TeacherModel(Model):
 					output = output[:output.index('\n')].strip() if '\n' in output else output.strip()
 					prediction = output.split(" ")[0]
 				print('%s Prediction = %s' % (self._model_name, prediction))
-
+			
 			elif self._task == "gsm8k":
 				prediction = re.sub(r"[^0-9.]", "", prediction)
 				if prediction == "" or prediction == ".":
@@ -147,9 +147,9 @@ class TeacherModel(Model):
 						if bool(re.search(r"\d", word)):
 							prediction = re.sub(r"[^0-9.]", "", word)
 							break
-
+		
 		return prediction, explanation
-
+	
 	def predict_batch(self, test_samples: List[Dict]) -> Tuple[List, List]:
 		predictions = []
 		explanations = []
@@ -157,5 +157,5 @@ class TeacherModel(Model):
 			prediction, explanation = self.predict(test_sample=test_sample)
 			predictions.append(prediction)
 			explanations.append(explanation)
-
+		
 		return predictions, explanations

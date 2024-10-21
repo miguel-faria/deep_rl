848246826abacbf42cbbdf9955200682922a0418
Miguel Faria <miguel.faria@tecnico.ulisboa.pt>
2024-10-21 11:11:04 +0100

    Update to miniconda script to allow for custom conda install dir

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+if [ $# -eq 0]; then
+  conda_home="$HOME"/miniconda3/
+else
+  conda_home=$1
+fi
+
+echo "export CONDA_HOME=""$conda_home" >> "$HOME"/.bashrc
+echo "\n" >> "$HOME"/.bashrc
+
 # Download and install miniconda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+mkdir -p "$conda_home"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$conda_home"/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p "$conda_home"
+rm -rf "$conda_home"/miniconda.sh
 source ~/.bashrc
 
 # Initialize conda
-~/miniconda3/bin/conda init bash
+"$conda_home"/bin/conda init bash
 source ~/.bashrc
 
 # Install and initialize mamba 

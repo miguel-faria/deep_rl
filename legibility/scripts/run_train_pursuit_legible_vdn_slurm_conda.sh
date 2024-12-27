ed98950f715a68143363e284e5d3ffed77be5c00
miguel-faria <miguel.faria@tecnico.ulisboa.pt>
2024-12-27 13:20:46 +0000

    Pursuit train merge changes

--- legibility/scripts/run_train_pursuit_legible_vdn_slurm_conda.sh
+++ legibility/scripts/run_train_pursuit_legible_vdn_slurm_conda.sh
@@ -37,7 +37,7 @@ fi
 
 source "$conda_dir"/bin/activate drl_env
 if [ "$HOSTNAME" = "artemis" ] || [ "$HOSTNAME" = "poseidon" ] ; then
-  python "$script_path"/run_train_pursuit_legible_vdn_dqn.py --field-len 10 --hunters 2 --prey-type idle --catch-reward 5 --batch-size 64 --buffer-size 5000 --iterations 35000 --episode-steps 800 --warmup 800 --limits 1 4 --eps-type linear --eps-decay 0.17 --legible-reward info --beta 0.9 --legibility-temp 0.1 --online-lr 0.00005 --use-higher-curriculum --logs-dir /mnt/scratch-artemis/miguelfaria/logs/pursuit --models-dir /mnt/data-artemis/miguelfaria/deep_rl/models --data-dir /mnt/data-artemis/miguelfaria/deep_rl/data
+  python "$script_path"/run_train_pursuit_legible_vdn_dqn.py --field-len 10 --hunters 3 --prey-type idle --catch-reward 5 --batch-size 64 --buffer-size 5000 --iterations 35000 --episode-steps 800 --warmup 800 --limits 1 3 --eps-type linear --eps-decay 0.17 --legible-reward q_vals --beta 0.9 --legibility-temp 0.1 --online-lr 0.00005 --use-higher-curriculum --logs-dir /mnt/scratch-artemis/miguelfaria/logs/pursuit --models-dir /mnt/data-artemis/miguelfaria/deep_rl/models --data-dir /mnt/data-artemis/miguelfaria/deep_rl/data
 elif [ "$HOSTNAME" = "hera" ] ; then
   nohup python "$script_path"/run_train_pursuit_legible_vdn_dqn.py --field-len 10 --hunters 2 --prey-type idle --catch-reward 5 --batch-size 64 --buffer-size 5000 --iterations 15000 --episode-steps 1000 --warmup 1000 --limits 1 4 --eps-type linear --eps-decay 0.27 --legible-reward info --beta 0.8 --legibility-temp 0.1 --online-lr 0.00005 --use-higher-curriculum --logs-dir /mnt/data-hera1/miguelfaria/deep_rl/logs/pursuit --models-dir /mnt/data-hera1/miguelfaria/deep_rl/models --data-dir /mnt/data-hera1/miguelfaria/deep_rl/data
 else

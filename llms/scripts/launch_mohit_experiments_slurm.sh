c787c0eb626d7060176f4dffd2f33e85f73b5e96
miguel-faria <miguel.faria@tecnico.ulisboa.pt>
2025-01-07 20:28:38 +0000

    Update mohit script launch to fix 404 error

--- llms/scripts/launch_mohit_experiments_slurm.sh
+++ llms/scripts/launch_mohit_experiments_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=miguel.faria@tecnico.ulisboa.pt
 #SBATCH --job-name=mohit_explanation_exec
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:3
 #SBATCH --time=4:00:00
